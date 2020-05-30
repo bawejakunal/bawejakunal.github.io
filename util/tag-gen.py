6187795397abf100b9889cc200e1f943abf1bb72
Kunal Baweja <bawejakunal@users.noreply.github.com>
2020-05-30 10:41:30 -0700

    update tag-gen.py script to support tags generation
    Closes #2

--- util/tag-gen.py
+++ util/tag-gen.py
@@ -12,44 +12,54 @@ No plugins required.
 
 import glob
 import os
+import subprocess
 
-POST_DIR = '../_posts/{}'
-TAG_DIR = '../_tag/'
+def main():
 
-FILENAMES = glob.glob(POST_DIR.format('*md'))
+    GIT_ROOT = subprocess.check_output(['git', 'rev-parse', '--show-toplevel']).strip()
+    POST_DIR = GIT_ROOT + '/_posts/{}'
+    TAG_DIR = GIT_ROOT + '/_tag/'
 
-total_tags = []
-for filename in FILENAMES:
-    f = open(filename, 'r')
-    crawl = False
-    for line in f:
-        if crawl:
-            current_tags = line.strip().split()
-            if current_tags[0] == 'tags:':
-                total_tags.extend(current_tags[1:])
-                crawl = False
-                break
-        if line.strip() == '---':
-            if not crawl:
-                crawl = True
-            else:
-                crawl = False
-                break
-    f.close()
-total_tags = set(total_tags)
+    FILENAMES = glob.glob(POST_DIR.format('*md'))
 
-old_tags = glob.glob(TAG_DIR + '*.md')
-for tag in old_tags:
-    os.remove(tag)
+    total_tags = []
+    for filename in FILENAMES:
+        f = open(filename, 'r')
+        crawl = False
+        for line in f:
+            if crawl:
+                current_tags = line.strip().split()
+                if current_tags[0] == 'tags:':
+                    total_tags.extend(current_tags[1:])
+                    crawl = False
+                    break
+            if line.strip() == '---':
+                if not crawl:
+                    crawl = True
+                else:
+                    crawl = False
+                    break
+        f.close()
+    total_tags = set(total_tags)
 
-for tag in total_tags:
-    tag_filename = TAG_DIR + tag + '.md'
-    f = open(tag_filename, 'w')
-    write_str = """---\ntag: {}
-feature_text: |
-    <h2 class="whitetext">Tag: {}</h2>
----
-""".format(tag, tag)
-    f.write(write_str)
-    f.close()
-print("Tags generated, count", total_tags.__len__())
+    old_tags = glob.glob(TAG_DIR + '*.md')
+    for tag in old_tags:
+        os.remove(tag)
+
+    for tag in total_tags:
+        tag_filename = TAG_DIR + tag + '.md'
+        f = open(tag_filename, 'w')
+        write_str = "---\n" \
+                    "tag: {0}\n" \
+                    "feature_text: |\n" \
+                    "    <h2 class=\"whitetext\">Tag: {0}</h2>\n" \
+                    "---"\
+                    .format(tag)
+        
+        f.write(write_str)
+        f.close()
+    print("Tags generated, count", total_tags.__len__())
+
+
+if __name__ == '__main__':
+    main()
